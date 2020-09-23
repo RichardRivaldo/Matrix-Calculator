7f7ec5b14037e25d170cb8ca7ccb56429fa243dc
solace6282 <gkataro4@gmail.com>
2020-09-23 23:04:15 +0700

    error dikit, sudah fix

--- src/Determinan.java
+++ src/Determinan.java
@@ -60,6 +60,6 @@ public class Determinan {
         Matrix M = new Matrix(1,1);
         M = Matrix.MakeMatrix();
 
-        System.out.println(hitungDeterminan(M));
+        System.out.println(hitungDeterminanEK(M));
     }
 }
