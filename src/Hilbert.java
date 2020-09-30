ce2264b6c0000d94b4d4d3c75850747362e4c1eb
solace6282 <gkataro4@gmail.com>
2020-10-01 00:34:49 +0700

    fixes

--- src/Hilbert.java
+++ src/Hilbert.java
@@ -1,13 +1,32 @@
 import java.util.Scanner;
 
 public class Hilbert {
+    public static Matrix MHilbert(int a) {
+        Matrix M = new Matrix(a,a+1);
+        int counter = 0;
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a+1; j++) {
+                double diva = j+1+counter;
+                double d = 1/diva;
+                if (j == a){
+                    Matrix.SetElmt(M, i, j, 0);
+                }
+                else{
+                    Matrix.SetElmt(M, i, j, d);
+                }
+            }
+            counter++;
+        }
+        Matrix.SetElmt(M, 0, a, 1);
+        return M;
+    }
     public static void main(String[] args) {
         Matrix A;
         Scanner sc = new Scanner(System.in);
         
         System.out.print("Masukkan nilai n untuk Hilbert: ");
         int a = sc.nextInt();
-        A = SPL.Hilbert(a); 
+        A = MHilbert(a); 
         System.out.println("Berikut matrix anda: ");
         Matrix.TulisMatrix(A);
 
